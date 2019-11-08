2d2e31046608a59240aab5ee5644d632e3bb683e
surya khanna <surya.khanna@simplilearn.net>
2019-11-08 18:32:59 +0530

    basic programs updated

---
+++ src/main/java/example/programming/interview/MostFrequentlyOccuringItem.java
@@ -0,0 +1,10 @@
+package example.programming.interview;
+
+public class MostFrequentlyOccuringItem {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/main/java/example/programming/interview/RotationProblem.java
+++ src/main/java/example/programming/interview/RotationProblem.java
@@ -8,5 +8,32 @@ package example.programming.interview;
  *
  */
 public class RotationProblem {
+	
+	
+	public static void main(String args[])
+	{
+		String s = "water";
+		//String s1 = "rwater"; // not a rotation
+		//String s1 = "rwate";  // rotation
+		//String s1 = "rw123";  // not a rotation
+		String s1 = "rwtae";	// not a rotation
+		
+		
+		String concate = s+s;  //concatenate parent string to itself
+		
+		if(s.length() == s1.length())
+		{
+			if(concate.contains(s1))
+				System.out.println(s1+" is a rotation of "+s);
+			else
+				System.out.println("Not a rotation");
+		}
+		else
+		{
+			System.out.println("Strings are not equal, hence not a rotation");
+		}
+		
+	}
+	
 
 }
